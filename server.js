e2fae1493d15f5a45033992db3d8c017ee0dced1
Rochelle Valdez <rochellevee@gmail.com>
2017-08-19 19:12:26 -0700

    Set up server and dependencies
    Modified/Added following files:
    -server.js
    -package.json
    -node modules

---
+++ server.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const express = require('express');
+const https = require('https');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+
+const key = fs.readFileSync('journey-key.pem');
+const cert = fs.readFileSync('journey-cert.pem');
+const https_options = {
+    key: key,
+    cert: cert
+};
+
+const app = express();
+const port = process.env.PORT || 8080;
+const server = https.createServer(https_options, app).listen(port, function() {
+  console.log('listening on port ' + port)
+})
+
+//MIDDLEWARE
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
